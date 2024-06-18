6770868a287907fff045eb5231f6693a136db9cd
Udon-Tobira <146440502+Udon-Tobira@users.noreply.github.com>
2024-06-18 18:53:14 +0900

    ThirdPersonCharacterに書いていたロジックをViewCaptureComponentに移動して、それをプラグインにした

--- Source/ViewportCaptureBy2D/Private/ExecuteSystemCommand.cpp
+++
@@ -1,7 +0,0 @@
-// Fill out your copyright notice in the Description page of Project Settings.
-
-#include "ExecuteSystemCommand.h"
-
-void UExecuteSystemCommand::ExecuteSystemCommand(const FString& Command) {
-	std::system(TCHAR_TO_ANSI(*Command));
-}
